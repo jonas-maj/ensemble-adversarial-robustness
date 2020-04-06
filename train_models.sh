a6b8e0b3548bb009a7243e7091875fcf4289959f
Mainuddin Ahmad Jonas <maj2bh@virginia.edu>
2020-04-06 19:54:47 -0400

    Enable all model training

--- train_models.sh
+++ train_models.sh
@@ -2,51 +2,51 @@
 
 cd examples/
 
-# python3 mnist_task_spec.py --category even --epochs=2
-# python3 mnist_task_spec.py --category odd
-# python3 mnist_task_spec.py --category even_target
-# python3 mnist_task_spec.py --category odd_target
-# python3 mnist_task_spec.py --category even_opt
-# python3 mnist_task_spec.py --category odd_opt
-# python3 mnist_task_spec.py --category even_opt_target
+python3 mnist_task_spec.py --category even
+python3 mnist_task_spec.py --category odd
+python3 mnist_task_spec.py --category even_target
+python3 mnist_task_spec.py --category odd_target
+python3 mnist_task_spec.py --category even_opt
+python3 mnist_task_spec.py --category odd_opt
+python3 mnist_task_spec.py --category even_opt_target
 python3 mnist_task_spec.py --category odd_opt_target
-# python3 mnist_task_spec.py --category mod5_0
-# python3 mnist_task_spec.py --category mod5_1
-# python3 mnist_task_spec.py --category mod5_2
-# python3 mnist_task_spec.py --category mod5_3
-# python3 mnist_task_spec.py --category mod5_4
-# python3 mnist_task_spec.py --category mod5_0_target
-# python3 mnist_task_spec.py --category mod5_1_target
-# python3 mnist_task_spec.py --category mod5_2_target
-# python3 mnist_task_spec.py --category mod5_3_target
-# python3 mnist_task_spec.py --category mod5_4_target
-# python3 mnist_task_spec.py --category opt5_1
-# python3 mnist_task_spec.py --category opt5_2
-# python3 mnist_task_spec.py --category opt5_3
-# python3 mnist_task_spec.py --category opt5_4
-# python3 mnist_task_spec.py --category opt5_0_target
-# python3 mnist_task_spec.py --category opt5_1_target
-# python3 mnist_task_spec.py --category opt5_2_target
-# python3 mnist_task_spec.py --category opt5_3_target
-# python3 mnist_task_spec.py --category opt5_4_target
-# python3 mnist_task_spec.py --category mod10_0
-# python3 mnist_task_spec.py --category mod10_1
-# python3 mnist_task_spec.py --category mod10_2
-# python3 mnist_task_spec.py --category mod10_3
-# python3 mnist_task_spec.py --category mod10_4
-# python3 mnist_task_spec.py --category mod10_5
-# python3 mnist_task_spec.py --category mod10_6
-# python3 mnist_task_spec.py --category mod10_7
-# python3 mnist_task_spec.py --category mod10_8
-# python3 mnist_task_spec.py --category mod10_9
-# python3 mnist_task_spec.py --category mod10_0_target
-# python3 mnist_task_spec.py --category mod10_1_target
-# python3 mnist_task_spec.py --category mod10_2_target
-# python3 mnist_task_spec.py --category mod10_3_target
-# python3 mnist_task_spec.py --category mod10_4_target
-# python3 mnist_task_spec.py --category mod10_5_target
-# python3 mnist_task_spec.py --category mod10_6_target
-# python3 mnist_task_spec.py --category mod10_7_target
-# python3 mnist_task_spec.py --category mod10_8_target
-# python3 mnist_task_spec.py --category mod10_9_target
+python3 mnist_task_spec.py --category mod5_0
+python3 mnist_task_spec.py --category mod5_1
+python3 mnist_task_spec.py --category mod5_2
+python3 mnist_task_spec.py --category mod5_3
+python3 mnist_task_spec.py --category mod5_4
+python3 mnist_task_spec.py --category mod5_0_target
+python3 mnist_task_spec.py --category mod5_1_target
+python3 mnist_task_spec.py --category mod5_2_target
+python3 mnist_task_spec.py --category mod5_3_target
+python3 mnist_task_spec.py --category mod5_4_target
+python3 mnist_task_spec.py --category opt5_1
+python3 mnist_task_spec.py --category opt5_2
+python3 mnist_task_spec.py --category opt5_3
+python3 mnist_task_spec.py --category opt5_4
+python3 mnist_task_spec.py --category opt5_0_target
+python3 mnist_task_spec.py --category opt5_1_target
+python3 mnist_task_spec.py --category opt5_2_target
+python3 mnist_task_spec.py --category opt5_3_target
+python3 mnist_task_spec.py --category opt5_4_target
+python3 mnist_task_spec.py --category mod10_0
+python3 mnist_task_spec.py --category mod10_1
+python3 mnist_task_spec.py --category mod10_2
+python3 mnist_task_spec.py --category mod10_3
+python3 mnist_task_spec.py --category mod10_4
+python3 mnist_task_spec.py --category mod10_5
+python3 mnist_task_spec.py --category mod10_6
+python3 mnist_task_spec.py --category mod10_7
+python3 mnist_task_spec.py --category mod10_8
+python3 mnist_task_spec.py --category mod10_9
+python3 mnist_task_spec.py --category mod10_0_target
+python3 mnist_task_spec.py --category mod10_1_target
+python3 mnist_task_spec.py --category mod10_2_target
+python3 mnist_task_spec.py --category mod10_3_target
+python3 mnist_task_spec.py --category mod10_4_target
+python3 mnist_task_spec.py --category mod10_5_target
+python3 mnist_task_spec.py --category mod10_6_target
+python3 mnist_task_spec.py --category mod10_7_target
+python3 mnist_task_spec.py --category mod10_8_target
+python3 mnist_task_spec.py --category mod10_9_target
 
